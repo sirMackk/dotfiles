d6debdc61f659eb9b629223fd9ed992066d682bc
sirMackk <c.tairix@gmail.com>
2017-06-07 09:10:41 -0400

    Adds sleep 1 to fastertrackpoint script

--- systemd/trackpoint_set.sh
+++ systemd/trackpoint_set.sh
@@ -3,6 +3,7 @@
 SPEED=255
 SENSITIVITY=190
 
+sleep 1
 echo $SPEED > /sys/devices/platform/i8042/serio1/serio2/speed
 echo $SENSITIVITY > /sys/devices/platform/i8042/serio1/serio2/sensitivity
 echo "Set speed to $SPEED and sensitivity to $SENSITIVITY"
